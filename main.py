69189329af1e34eb4dada2ff5d333850d800b6b5
esteban-elias <esteban.elias@proton.me>
2024-08-08 01:34:24 -0400

    Add guidelines

--- main.py
+++ main.py
@@ -13,7 +13,13 @@ def append_ai_message(message: str, history: str):
     return history + 'AI: ' + message + '\n'
 
 
-history = ''
+history = '''
+Guidelines of this conversation:
+- If you do not find information, please be
+honest and do not invent information and do not hallucinate.
+- You are "AI" an I am "HUMAN".
+Now, let's start the conversation.
+'''
 
 command = sys.argv[1:-1]
 first_prompt = sys.argv[-1]
@@ -23,32 +29,21 @@ history = append_human_message(first_prompt, history)
 console = Console()
 
 while True:
-    # write to history.txt
-    with open('history.txt', 'w') as f:
-        f.write(history)
     try:
         # Start the subprocess with line buffering
         process = subprocess.Popen(
-            command+[history], stdout=subprocess.PIPE, bufsize=1, universal_newlines=True)
+            command+[history], stdout=subprocess.PIPE, text=True)
 
-        stdout = ''
+        stdout, _ = process.communicate()
 
-        # Loop over the lines from the subprocess's stdout
         print()
-        for line in process.stdout:
-            line = line.strip()
-            stdout += line + '\n'
-
         console.print(Markdown(stdout))
-
-        # Wait for the subprocess to finish
-        process.wait()
 
         history = append_ai_message(stdout, history=history)
 
         prompt = Prompt.ask("[bold green]\n>>>[/bold green]")
         history = append_human_message(prompt, history=history)
 
-    except KeyboardInterrupt or EOFError:
+    except (KeyboardInterrupt, EOFError):
         print('Goodbye!')
         break
